7681dc5f9c6ee0283466fc5a334c2950ada58a29
mikeatgl <speakoftrees@gmail.com>
2017-08-11 00:17:05 -0400

    Just got JSON server running and starting on adding companies model.

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,5 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const {
 	GraphQLObjectType,
 	GraphQLString,
@@ -7,21 +7,22 @@ const {
 	GraphQLSchema
 } = graphql;
 
-const users = [
-	{
-		id: '23', firstName: 'Bill', age: 20
-	},
-	{
-		id: '47', firtName: 'Samantha', age: 21
-	}
-];
-
 const UserType = new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		companyId: { type: GraphQLString }
+	}
+});
+
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString }
 	}
 });
 
@@ -32,8 +33,16 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: { id: { type: GraphQLString } },
 			resolve(parentValue, args) {
-				return _.find(users, { id: args.id });
+				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data);
 			}
+		},
+		company: {
+			type: CompanyType,
+			args: { id: { type: GraphQLString } },
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
+					.then(resp => resp.data);			}
 		}
 	}
 });
